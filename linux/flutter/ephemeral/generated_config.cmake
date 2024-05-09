78c0b758bd125cbc36acefb28d696946093fd7d5
NurkholikSafrudin <149698766+NurkholikSafrudin@users.noreply.github.com>
2024-05-09 20:24:12 +0700

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/jati/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/jati/Documents/aplikasi_flutter_pertamaku" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/jati/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/jati/Documents/aplikasi_flutter_pertamaku"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iNGJmZDQ1OTg2NWE4ZDYzNmYyNmFjYTBkMzMwYWUyOTdjMDA2YzNjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/jati/Documents/aplikasi_flutter_pertamaku/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/jati/Documents/aplikasi_flutter_pertamaku/lib/main.dart"
+)
